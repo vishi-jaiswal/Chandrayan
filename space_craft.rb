a53e09163666a4b790c62709f2e275975301b8d3
Vishi Jaiswal <vishi.jaiswal@planetspark.in>
2024-02-04 19:32:09 +0530

    with wrong result

--- space_craft.rb
+++ space_craft.rb
@@ -3,4 +3,25 @@ class SpaceCraft
     @position = starting_point
     @direction = initial_direction
   end
+
+  def move_forward
+    case @direction
+    when 'N'
+      @position[1] += 1
+    when 'S'
+      @position[1] -= 1
+    when 'E'
+      @position[0] += 1
+    when 'W'
+      @position[0] -= 1
+    when 'Up'
+      @position[2] += 1
+    when 'Down'
+      @position[2] -= 1
+    end
+  end
+
+  def final_position_and_direction
+    { position: @position, direction: @direction }
+  end
 end
--- space_craft.rspec.rb
+++ space_craft.rspec.rb
@@ -5,7 +5,7 @@ RSpec.describe SpaceCraft do
     it "moves the spacecraft forward" do
     	spacecraft = SpaceCraft.new([0, 0, 0], 'N')
     	spacecraft.move_forward
-      expect(spacecraft.final_position_and_direction[:position]).to eq([0, 1, 0])
+      expect(spacecraft.final_position_and_direction[:position]).to eq([0, 0, 0])
     end
   end
 end
