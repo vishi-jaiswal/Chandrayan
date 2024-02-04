8daa6a348e7bd4e246dfa65bed5ff21a4be7cd69
Vishi Jaiswal <vishi.jaiswal@planetspark.in>
2024-02-04 19:35:41 +0530

    same as it all the test cases will be checked followed by TDD Approach

--- space_craft.rb
+++ space_craft.rb
@@ -20,7 +20,101 @@ class SpaceCraft
       @position[2] -= 1
     end
   end
+  def move_forward
+    case @direction
+    when 'N'
+      @position[1] += 1
+    when 'S'
+      @position[1] -= 1
+    when 'E'
+      @position[0] += 1
+    when 'W'
+      @position[0] -= 1
+    when 'Up'
+      @position[2] += 1
+    when 'Down'
+      @position[2] -= 1
+    end
+  end
 
+  def move_backward
+    case @direction
+    when 'N'
+      @position[1] -= 1
+    when 'S'
+      @position[1] += 1
+    when 'E'
+      @position[0] -= 1
+    when 'W'
+      @position[0] += 1
+    when 'Up'
+      @position[2] -= 1
+    when 'Down'
+      @position[2] += 1
+    end
+  end
+
+  def turn_left
+    case @direction
+    when 'N'
+      @direction = 'W'
+    when 'S'
+      @direction = 'E'
+    when 'E'
+      @direction = 'N'
+    when 'W'
+      @direction = 'S'
+    when 'Up'
+      @direction = 'N'
+    when 'Down'
+      @direction = 'S'  
+    end
+  end
+
+  def turn_right
+    case @direction
+    when 'N'
+      @direction = 'E'
+    when 'S'
+      @direction = 'W'
+    when 'E'
+      @direction = 'S'
+    when 'W'
+      @direction = 'N'
+    end
+  end
+
+  def turn_up
+    return unless @direction == 'N' || @direction == 'E'
+
+    @direction = 'Up'
+  end
+
+  def turn_down
+    return unless @direction == 'N' || @direction == 'S'
+
+    @direction = 'Down'
+  end
+
+  def process_commands(commands)
+    commands.each do |command|
+      case command
+      when 'f'
+        move_forward
+      when 'b'
+        move_backward
+      when 'l'
+        turn_left
+      when 'r'
+        turn_right
+      when 'u'
+        turn_up
+      when 'd'
+        turn_down
+      end
+      puts "\"#{command}\" - #{@position} - #{@direction}"
+    end
+  end
   def final_position_and_direction
     { position: @position, direction: @direction }
   end
--- space_craft.rspec.rb
+++ space_craft.rspec.rb
@@ -8,4 +8,52 @@ RSpec.describe SpaceCraft do
       expect(spacecraft.final_position_and_direction[:position]).to eq([0, 1, 0])
     end
   end
+  describe "#move_backward" do
+    it "moves the spacecraft backward" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.move_backward
+      expect(spacecraft.final_position_and_direction[:position]).to eq([0, -1, 0])
+    end
+  end
+
+  describe "#turn_left" do
+    it "turns the spacecraft left" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.turn_left
+      expect(spacecraft.final_position_and_direction[:direction]).to eq('W')
+    end
+  end
+
+  describe "#turn_right" do
+    it "turns the spacecraft right" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.turn_right
+      expect(spacecraft.final_position_and_direction[:direction]).to eq('E')
+    end
+  end
+
+  describe "#turn_up" do
+    it "turns the spacecraft up" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.turn_up
+      expect(spacecraft.final_position_and_direction[:direction]).to eq('Up')
+    end
+  end
+
+  describe "#turn_down" do
+    it "turns the spacecraft down" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.turn_down
+      expect(spacecraft.final_position_and_direction[:direction]).to eq('Down')
+    end
+  end
+
+  describe "#process_commands" do
+    it "processes a sequence of commands" do
+      spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+      spacecraft.process_commands(['f', 'r', 'u', 'b', 'l'])
+      expect(spacecraft.final_position_and_direction[:position]).to eq([0, 1, -1])
+      expect(spacecraft.final_position_and_direction[:direction]).to eq('N')
+    end
+  end
 end
