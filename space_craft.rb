272cb1b099f7e1230e6a80bfc8b3ea0d6d8beb1d
Vishi Jaiswal <vishi.jaiswal@planetspark.in>
2024-02-04 19:29:13 +0530

    no method defined

---
+++ space_craft.rb
@@ -0,0 +1,2 @@
+class SpaceCraft
+end
--- space_craft.rspec.rb
+++ space_craft.rspec.rb
@@ -3,6 +3,9 @@ require_relative 'space_craft'
 RSpec.describe SpaceCraft do
   describe "#move_forward" do
     it "moves the spacecraft forward" do
+    	spacecraft = SpaceCraft.new([0, 0, 0], 'N')
+    	spacecraft.move_forward
+      expect(spacecraft.final_position_and_direction[:position]).to eq([0, 1, 0])
     end
   end
 end
